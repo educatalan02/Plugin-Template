c96d2511212946302e1fec8a5ed336960048c007
Coolpuppy24 <pat.pvp.unturned@gmail.com>
2017-04-18 18:40:59 -0500

    ..

---
+++ Commands/Command.cs
@@ -0,0 +1,66 @@
+﻿using Rocket.API;
+using Rocket.Core.Logging;
+using Rocket.Unturned.Chat;
+using Rocket.Unturned.Player;
+using System;
+using System.Collections.Generic;
+
+namespace name.pluginname
+{
+    public class Command : IRocketCommand
+    {
+        public static Main Instance;
+
+        public List<string> Aliases
+        {
+            get
+            {
+                return new List<string>() {};
+            }
+        }
+
+        public AllowedCaller AllowedCaller
+        {
+            get
+            {
+                return AllowedCaller.Player;
+            }
+        }
+
+        public string Help
+        {
+            get
+            {
+                return "";
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return "";
+            }
+        }
+
+        public List<string> Permissions
+        {
+            get
+            {
+                return new List<string>() { "" };
+            }
+        }
+
+        public string Syntax
+        {
+            get
+            {
+                return "";
+            }
+        }
+
+        public void Execute(IRocketPlayer caller, string[] command)
+        {
+        }
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using Rocket.API;
+using Rocket.Unturned.Player;
+using UnityEngine;
+using SDG.Unturned;
+using Rocket.Core.Plugins;
+using Rocket.Core.Logging;
+using Rocket.API.Collections;
+using Rocket.Unturned.Chat;
+using Rocket.Core;
+using System.Collections.Generic;
+
+namespace name.pluginname
+{
+    public class Main : RocketPlugin
+    {
+        public static Main Instance = null;
+
+
+        protected override void Load()
+        {
+            Instance = this;
+
+            Rocket.Core.Logging.Logger.Log("Load");
+        }
+
+        protected override void Unload()
+        {
+            Rocket.Core.Logging.Logger.Log("Unload");
+        }
+    }
+}
